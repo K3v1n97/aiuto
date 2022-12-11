fc399680def5a9e35072e8df5480934782dd2684
K3v1n97 <115088415+K3v1n97@users.noreply.github.com>
2022-12-11 19:04:33 +0100

    resistors

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResistorsColor;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            String[] colors = { "Black", "Brown", "Red", "Orange", "Yellow", "Grren", "Blue", "Purple", "Gray", "White" };
+
+            foreach(string value in colors)
+            {
+                MyComboBox.Items.Add($"{value}");
+            }
+
+        }
+
+    private void MyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        MyTextBox.Text = MyComboBox.SelectedIndex.ToString();
+    }
+}
